0e2aeea02253de795c1366f8ec4d62e4b9688880
whiterabbit21m-test <152816037+whiterabbit21m-test@users.noreply.github.com>
2024-07-25 14:26:55 +0200

    Update shop.js

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
   async function createBTCPayInvoice(customerData) {
     const btcPayServerUrl = 'https://btcpay.whiterabbit21m.com';
     const storeId = '5vHj4TmiyYMCkFUpyBYf6rUDvaJ6YA7B74v2G7iYD9D2';
-    const apiKey = 'YOUR_API_KEY_HERE'; // Sostituisci con il tuo token API reale
+    const apiKey = '4a18abbdd5919b860a6dd4d58eb4238618bdaf22'; // Sostituisci con il tuo token API reale
 
     const invoiceData = {
       amount: calculateTotal(),
