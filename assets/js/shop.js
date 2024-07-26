02f53e43f68f4195bd2d35b40fa8384550a102e0
whiterabbit21m-test <152816037+whiterabbit21m-test@users.noreply.github.com>
2024-07-26 10:05:20 +0200

    Update shop.js

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -151,7 +151,7 @@ Country: ${customerData.country}
                 speedPolicy: 'HighSpeed',
                 paymentMethods: ['BTC', 'BTC-LightningNetwork'],
                 defaultPaymentMethod: 'BTC-LightningNetwork'
-                redirectURL: shopPath, // Aggiungi questa linea
+                redirectURL: 'shopPath' // Aggiungi questa linea
                 redirectAutomatically: true // Aggiungi questa linea per reindirizzare automaticamente
             },
             buyer: {
